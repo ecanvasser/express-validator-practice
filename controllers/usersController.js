b1f086ccd610073b5d7943270cde3bc0d532d318
Eddie Canvasser <eddiecanvassere9@gmail.com>
2024-08-07 01:14:42 -0700

    Set up boilerplate app from odin project

---
+++ controllers/usersController.js
@@ -0,0 +1,21 @@
+// controllers/usersController.js
+const usersStorage = require("../storages/usersStorage");
+
+exports.usersListGet = (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+};
+
+exports.usersCreateGet = (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+};
+
+exports.usersCreatePost = (req, res) => {
+  const { firstName, lastName } = req.body;
+  usersStorage.addUser({ firstName, lastName });
+  res.redirect("/");
+};
---
+++ routes/usersRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const usersController = require("../controllers/usersController");
+const usersRouter = Router();
+
+usersRouter.get("/", usersController.usersListGet);
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
+
+module.exports = usersRouter;
