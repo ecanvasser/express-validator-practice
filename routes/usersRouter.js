b88e555eb8c5ff5dc5677718e194ca0b6eb60df2
Eddie Canvasser <eddiecanvassere9@gmail.com>
2024-08-08 01:43:32 -0700

    Added full CRUD functionality

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,8 @@ const usersRouter = Router();
 usersRouter.get("/", usersController.usersListGet);
 usersRouter.get("/create", usersController.usersCreateGet);
 usersRouter.post("/create", usersController.usersCreatePost);
+usersRouter.get("/:id/update", usersController.usersUpdateGet);
+usersRouter.post("/:id/update", usersController.usersUpdatePost);
+usersRouter.post("/:id/delete", usersController.usersDeletePost);
 
 module.exports = usersRouter;
